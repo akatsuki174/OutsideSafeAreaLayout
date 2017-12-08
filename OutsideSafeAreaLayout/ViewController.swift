1399d5c9f02f5d6f737be0ace52783d51aff4c92
akatsuki174 <dev2015js@gmail.com>
2017-12-08 22:20:01 +0900

    UIViewControllerにtable viewを貼り付けた

---
+++ OutsideSafeAreaLayout/ViewController.swift
@@ -0,0 +1,46 @@
+import UIKit
+import PureLayout
+
+final class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+
+        let menuView = MenuView.instance()
+        self.view.addSubview(menuView)
+        menuView.autoAlignAxis(toSuperviewAxis: .vertical)
+        menuView.autoPinEdge(toSuperviewEdge: .bottom)
+        menuView.autoSetDimensions(to: CGSize.init(width: self.view.frame.width, height: 90))
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
+
+// MARK: UITableViewDelegate, UITableViewDataSource
+
+extension ViewController {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = String(indexPath.row)
+        return cell
+    }
+
+}
+
+
